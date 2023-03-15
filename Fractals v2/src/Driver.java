5e0f22f03699ed3e885b603f89fcaa619a4be3b1
Anish Gowda <Anish Gowda@DESKTOP-UMISK5B.lan>
2023-03-14 22:37:13 -0700

    Circles and Sierpinski Triangle

--- Fractals v2/src/Driver.java
+++ Fractals v2/src/Driver.java
@@ -15,23 +15,71 @@ public class Driver extends JPanel implements ActionListener {
 
 	public void paint(Graphics g) {
 		super.paintComponent(g);
-		rings(g, 200, 0, 0);
+		
+		//Rings cuhhhhhh
+//		rings(g, 200, 0, 200);
+//		rings1(g, 200, 0, 0);
+//		rings2(g, 200, 200, 200);
+//		rings3(g, 200, 200, 0);
+		
+		
+		//serpinski triyangle
+		//SierpinskiTriangle(g, 200, 500, 250);
+		
 
 	}// end of paint method - put code above for anything dealing with drawing -
 
 	public void clover(Graphics g, int radius, int x, int y) {
-// need this code! What a test hahahahahahahahaha
-		System.out.println("amongus is a bad game");
+
+		
 	
 
 	}
+	public void SierpinskiTriangle(Graphics g, double x, double y, double size) {
+        if(size < 1) return;
+        
+        g.setColor(Color.PINK);
+        g.drawLine((int)x, (int)y, (int)(x+size),(int)y);
+        g.drawLine((int)x, (int)y, (int)(x+size/2), (int)(y-size/21.732));
+        g.drawLine((int)(x+size), (int)y, (int)(x+size/2), (int)(y-size/21.732));
+        SierpinskiTriangle(g, x, y, size/2);
+        SierpinskiTriangle(g, x+size/4, (int)(y-size/4*1.732), size/2);
+        SierpinskiTriangle(g, x+size/2, y, size/2);
+    }
 
 	public void rings(Graphics g, int radius, int x, int y) {
 		// each method call draws one part of the fractal
 		g.drawOval(x, y, radius, radius);
+		if(radius>1) {
+			rings(g, radius-8, x+8, y);
+		}
 
 	}
+	public void rings1(Graphics g, int radius, int x, int y) {
+		// each method call draws one part of the fractal
+		g.drawOval(x, y, radius, radius);
+		if(radius>1) {
+			rings(g, radius-8, x+8, y+8);
+		}
 
+	}
+	
+	public void rings2(Graphics g, int radius, int x, int y) {
+		// each method call draws one part of the fractal
+		g.drawOval(x, y, radius, radius);
+		if(radius>1) {
+			rings(g, radius-8, x, y);
+		}
+
+	}
+	public void rings3(Graphics g, int radius, int x, int y) {
+		// each method call draws one part of the fractal
+		g.drawOval(x, y, radius, radius);
+		if(radius>1) {
+			rings(g, radius, x, y);
+		}
+
+	}
 	/**
 	 * Update the positions of the ball and paddle. Update the scores, counter
 	 * and time
